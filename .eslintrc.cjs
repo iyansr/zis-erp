3f48cacbc08e36be1afd88486eef130459b75c1b
iyansr <ikhyan7@gmail.com>
2023-11-01 17:41:55 +0800

    Init

---
+++ .eslintrc.cjs
@@ -0,0 +1,21 @@
+module.exports = {
+  root: true,
+  env: { browser: true, es2020: true },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react/jsx-runtime',
+    'plugin:react-hooks/recommended',
+    'prettier',
+  ],
+  ignorePatterns: ['dist', '.eslintrc.cjs', 'postcss.config.cjs'],
+  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
+  settings: { react: { version: '18.2' } },
+  plugins: ['react-refresh', 'simple-import-sort', 'prettier', 'react'],
+  rules: {
+    'react-refresh/only-export-components': ['warn', { allowConstantExport: true }],
+    'simple-import-sort/imports': 'warn',
+    'prettier/prettier': 'error',
+    'tailwindcss/no-custom-classname': 0,
+  },
+};
---
+++ src/App.jsx
@@ -0,0 +1,5 @@
+const Hello = () => {
+  return <div className="text-red-500">Hello</div>;
+};
+
+export default Hello;
